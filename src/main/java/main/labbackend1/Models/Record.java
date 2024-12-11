b5a39e45c04c0588392f1f2c6df592f4a556aeec
karoll1naa <karollinnajan@gmail.com>
2024-12-11 17:49:34 +0200

    Added Record class

--- src/main/java/main/labbackend1/Models/Record.java
+++ src/main/java/main/labbackend1/Models/Record.java
@@ -1,4 +1,42 @@
 package main.labbackend1.Models;
 
+import java.time.LocalDateTime;
+
 public class Record {
+    private Long id;
+    private Long userId;
+    private Long categoryId;
+    private LocalDateTime created;
+    private Double amount;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public Long getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+    public LocalDateTime getCreated() {
+        return created;
+    }
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+    public Double getAmount() {
+        return amount;
+    }
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
