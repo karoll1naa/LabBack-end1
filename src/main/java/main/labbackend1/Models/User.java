1d0440af43c3b6e9643edc717f73807e6ab91c1f
karoll1naa <karollinnajan@gmail.com>
2024-12-11 17:50:41 +0200

    Added User class

--- src/main/java/main/labbackend1/Models/User.java
+++ src/main/java/main/labbackend1/Models/User.java
@@ -1,4 +1,19 @@
 package main.labbackend1.Models;
 
 public class User {
+    private Long id;
+    private String username;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
 }
