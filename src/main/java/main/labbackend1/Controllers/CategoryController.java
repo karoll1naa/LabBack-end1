8665de27c696de6f1cc7661fe52a97ee2332e8e9
karoll1naa <karollinnajan@gmail.com>
2024-12-11 17:48:24 +0200

    Added Category class

--- src/main/java/main/labbackend1/Controllers/CategoryController.java
+++ src/main/java/main/labbackend1/Controllers/CategoryController.java
@@ -1,4 +1,28 @@
 package main.labbackend1.Controllers;
 
+import main.labbackend1.Models.Category;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @PostMapping
+    public void createCategory(@RequestBody Category category) {
+        categories.add(category);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public void deleteCategory(@PathVariable Long categoryId) {
+        categories.removeIf(category -> category.getId().equals(categoryId));
+    }
 }
