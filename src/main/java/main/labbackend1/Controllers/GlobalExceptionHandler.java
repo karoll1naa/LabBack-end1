50ba64649ece87e3170bfb9772a4d9275efa5a4a
karoll1naa <karollinnajan@gmail.com>
2024-12-17 17:52:36 +0200

    Added error handler

---
+++ src/main/java/main/labbackend1/Controllers/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package main.labbackend1.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
