38cf69ed83c246a5d738ee0a81e124091784a573
karoll1naa <karollinnajan@gmail.com>
2024-12-17 17:54:23 +0200

    Added to the main scan to search for entities

--- src/main/java/main/labbackend1/LabBackEnd1Application.java
+++ src/main/java/main/labbackend1/LabBackEnd1Application.java
@@ -1,9 +1,13 @@
 package main.labbackend1;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan(basePackages = "main.labbackend2.Models")
+
 public class LabBackEnd1Application {
 
 	public static void main(String[] args) {
