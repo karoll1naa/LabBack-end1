d4f2b906770a5020d717eb2c6f44c303de4411ec
karoll1naa <karollinnajan@gmail.com>
2024-12-20 12:59:37 +0200

    Adding a request route

--- src/main/java/main/labbackend1/Controllers/AuthController.java
+++ src/main/java/main/labbackend1/Controllers/AuthController.java
@@ -1,4 +1,45 @@
 package main.labbackend1.Controllers;
+import main.labbackend1.Models.AuthRequest;
+import main.labbackend1.Models.AuthUser;
+import main.labbackend1.Repositories.AuthUserRepository;
+import main.labbackend1.Utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AuthUserRepository authUserRepository;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody AuthUser authUser) {
+        authUser.setPassword(passwordEncoder.encode(authUser.getPassword()));
+        authUserRepository.save(authUser);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+        String token = jwtUtil.generateToken(authRequest.getUsername());
+        return ResponseEntity.ok(token);
+    }
 }
+
