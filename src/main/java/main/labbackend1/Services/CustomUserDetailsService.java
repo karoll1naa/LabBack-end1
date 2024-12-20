0c958e3ce877743a0dfd84ac109fa159c0d969bd
karoll1naa <karollinnajan@gmail.com>
2024-12-20 12:53:20 +0200

    Adding a service

---
+++ src/main/java/main/labbackend1/Services/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package main.labbackend1.Services;
+import main.labbackend1.Models.AuthUser;
+import main.labbackend1.Repositories.AuthUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private AuthUserRepository authUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AuthUser user = authUserRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
